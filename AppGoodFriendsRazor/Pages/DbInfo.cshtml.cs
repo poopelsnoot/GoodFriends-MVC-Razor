aef2ae7a8d4e5e0ac1f5beb5fe04edebf973c69f
poopelsnoot <sofi.noctum@gmail.com>
2024-12-16 14:48:42 +0100

    feat: add nr friends and cities to html

--- AppGoodFriendsRazor/Pages/DbInfo.cshtml.cs
+++ AppGoodFriendsRazor/Pages/DbInfo.cshtml.cs
@@ -8,11 +8,16 @@ namespace MyApp.Namespace
     public class DbInfoModel : PageModel
     {
         readonly IFriendsService _service;
+
+        public int NrFriendsSweden { get; set; }
+        public int NrCitiesSweden { get; set; }
+        
         public async Task<IActionResult> OnGet()
         {
             GstUsrInfoAllDto dbInfo = await _service.InfoAsync;
 
-            var nrFriendsSweden = dbInfo.Friends.Where(f => f.Country == "Sweden").Sum(f => f.NrFriends);
+            NrFriendsSweden = dbInfo.Friends.Where(f => f.Country == "Sweden").Sum(f => f.NrFriends);
+            NrCitiesSweden = dbInfo.Friends.Count(f => f.Country == "Sweden");
             return Page();
         }
         
