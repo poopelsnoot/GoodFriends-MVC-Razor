1d11eda0604c425ff125b59e174281eb0ceadecc
poopelsnoot <sofi.noctum@gmail.com>
2024-12-16 13:38:55 +0100

    feat: add database connection and logger to razor

--- AppGoodFriendsRazor/Program.cs
+++ AppGoodFriendsRazor/Program.cs
@@ -1,7 +1,21 @@
+using Configuration;
+using DbContext;
+using DbRepos;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Configuration.AddApplicationSecrets("../Configuration/Configuration.csproj");
+builder.Services.AddDatabaseConnections(builder.Configuration);
+builder.Services.AddDatabaseConnectionsDbContext();
+
+//Inject Custom logger
+builder.Services.AddSingleton<ILoggerProvider, InMemoryLoggerProvider>();
+builder.Services.AddScoped<FriendsDbRepos>();
+builder.Services.AddScoped<IFriendsService, FriendsServiceDb>();
 
 var app = builder.Build();
 
