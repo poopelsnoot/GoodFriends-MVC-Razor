e3317d4e9f19905c20d94787c52624da52d69565
poopelsnoot <sofi.noctum@gmail.com>
2024-12-20 16:44:04 +0100

    feat: add pagination

--- AppGoodFriendsRazor/Pages/ListOfFriends.cshtml.cs
+++ AppGoodFriendsRazor/Pages/ListOfFriends.cshtml.cs
@@ -11,13 +11,18 @@ namespace MyApp.Namespace
         readonly IFriendsService _service;
         public List<IFriend> FriendsList { get; set; }
         public string ChosenCity { get; set; }
-        public async Task<IActionResult> OnGet(string city)
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public async Task<IActionResult> OnGet(string city, int page = 1)
         {
             GstUsrInfoAllDto dbInfo = await _service.InfoAsync;
             ChosenCity = city;
+            CurrentPage = page;
 
             var addresses = await _service.ReadAddressesAsync(true, false, city, 0, int.MaxValue);
-            FriendsList = addresses.PageItems.SelectMany(a => a.Friends).ToList();
+            FriendsList = addresses.PageItems.SelectMany(a => a.Friends).Skip((page-1) * 10).Take(10).ToList();
+
+            TotalPages = (int)Math.Ceiling((double)FriendsList.Count() / 10);
 
             return Page();
         }
