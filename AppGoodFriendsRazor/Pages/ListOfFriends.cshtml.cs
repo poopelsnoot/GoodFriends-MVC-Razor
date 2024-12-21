ce09aeb1a2a901ade6caf4edbcf05d0feebb216c
poopelsnoot <sofi.noctum@gmail.com>
2024-12-21 17:33:03 +0100

    feat: add search filter

--- AppGoodFriendsRazor/Pages/ListOfFriends.cshtml.cs
+++ AppGoodFriendsRazor/Pages/ListOfFriends.cshtml.cs
@@ -9,21 +9,34 @@ namespace MyApp.Namespace
     public class ListOfFriendsModel : PageModel
     {
         readonly IFriendsService _service;
-        public List<IFriend> FriendsList { get; set; }
+        private List<IFriend> AllFriends { get; set; } = new List<IFriend>();
+        public List<IFriend> FriendsList { get; set; } = new List<IFriend>();
         public string ChosenCity { get; set; }
         public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
+        public int TotalPages { get; set; } 
+
+        [BindProperty]
+        public string SearchFilter { get; set; }
         public async Task<IActionResult> OnGet(string city, int pageNumber = 1)
         {
             GstUsrInfoAllDto dbInfo = await _service.InfoAsync;
             ChosenCity = city;
             CurrentPage = pageNumber;
+            SearchFilter = Request.Query["search"];
+            if (SearchFilter == null) { SearchFilter = ""; }
 
-            var addresses = await _service.ReadAddressesAsync(true, false, city, 0, int.MaxValue);
-            var AllFriends = addresses.PageItems.SelectMany(a => a.Friends).ToList();
+            var Addresses = await _service.ReadAddressesAsync(true, false, city, 0, int.MaxValue);
+            AllFriends = Addresses.PageItems.SelectMany(a => a.Friends).ToList();
 
             TotalPages = (int)Math.Ceiling((double)AllFriends.Count() / 10);
-            FriendsList = AllFriends.Skip((pageNumber-1) * 10).Take(10).ToList();
+            FriendsList = AllFriends.Where(f => f.FirstName.Contains(SearchFilter) || f.LastName.Contains(SearchFilter)).Skip((CurrentPage-1) * 10).Take(10).ToList();
+
+            return Page();
+        }
+
+        public IActionResult OnPostSearch()
+        {
+            FriendsList = AllFriends.Where(f => f.FirstName.Contains(SearchFilter) || f.LastName.Contains(SearchFilter)).Take(10).ToList();
 
             return Page();
         }
