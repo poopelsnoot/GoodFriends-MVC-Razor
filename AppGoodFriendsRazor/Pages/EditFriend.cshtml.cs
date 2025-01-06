68c2b02e7763924b8fe3a9b2b3e2027d69d694e9
poopelsnoot <sofi.noctum@gmail.com>
2025-01-06 14:22:06 +0100

    feat: remove address fields if address is unknown

--- AppGoodFriendsRazor/Pages/EditFriend.cshtml.cs
+++ AppGoodFriendsRazor/Pages/EditFriend.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Eventing.Reader;
 using DbModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -15,6 +16,8 @@ namespace MyApp.Namespace
         public FriendCUdto FriendToEdit { get; set; }
         [BindProperty]
         public AddressCUdto AddressToEdit { get; set; }
+        [BindProperty]
+        public bool UserHasAddress { get; set; }
 
         public async Task<IActionResult> OnGet(Guid friendId)
         {
@@ -30,22 +33,25 @@ namespace MyApp.Namespace
                 AddressToEdit = new AddressCUdto();
             }
 
+            UserHasAddress = !string.IsNullOrEmpty(Friend.Address?.StreetAddress);
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostSave()
         {
-            if (AddressToEdit.AddressId == null)
+
+            if (AddressToEdit.AddressId == null && UserHasAddress)
             {
                 var newAddress = await _service.CreateAddressAsync(AddressToEdit);
                 AddressToEdit = new AddressCUdto(newAddress);
             }
-            else
+            else if (AddressToEdit.AddressId != null && UserHasAddress)
             {
                 await _service.UpdateAddressAsync(AddressToEdit);
             }
 
-            FriendToEdit.AddressId = AddressToEdit.AddressId;
+            if(UserHasAddress) { FriendToEdit.AddressId = AddressToEdit.AddressId; }
             await _service.UpdateFriendAsync(FriendToEdit);
             
             return Page();
